c4d0ebdbcc4e4bb762dcf9dfae97759f7ac0c083
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2016-02-04 19:31:41 +0200

    drawing lines and circles with a thickness

--- xsizero.lua
+++ xsizero.lua
@@ -130,12 +130,13 @@ end
 local app = nw:app()
 
 local win = app:window{
-   w = 400, h = 200,
+   cw = 400, ch = 300,
    title = 'X si Zero',
 	--visible = false,
 }
 
 function win:repaint()
+
 	local bmp = self:bitmap()
 	local p = ffi.cast('uint8_t*', bmp.data)
 
@@ -145,27 +146,66 @@ function win:repaint()
 		p[(y * bmp.w + x) * 4 + 0] = b
 	end
 
-	local function vline(x, y, length, r, g, b)
-		--
+	local function rectangle(x0, y0, w, h, r, g, b)
+		assert(x0 < bmp.w)
+		assert(y0 < bmp.h)
+		assert(x0 + w <= bmp.w)
+		assert(y0 + h <= bmp.h)
+		for y = y0, y0 + h - 1 do
+			for x = x0, x0 + w - 1 do
+				setpixel(x, y, r, g, b)
+			end
+		end
+	end
+
+	local function vline(x0, y0, length, thickness, r, g, b)
+		rectangle(x0 - math.floor(thickness / 2), y0, thickness, length, r, g, b)
 	end
 
 	local function hline(x, y, length, r, g, b)
-		--
+		rectangle(x0, y0 - math.floor(thickness / 2), length, thickness, r, g, b)
 	end
 
-	local function diagonal(x, y, bb_length, r, g, b)
-		--
-	end
-
-	local function circle(cx, cy, r)
-		--
-	end
-
-	for y=0,bmp.h-1 do
-		for x=0,bmp.w-1 do
-			p[(y * bmp.w + x) * 4 + 2] = 255
+	local function diagonal(x0, y0, bb_length, thickness, r, g, b)
+		local c = thickness
+		local a = math.sqrt(c^2 / 2)
+		local x1 = x0 + math.floor(a / 2)
+		local y1 = y0 - math.floor(a / 2)
+		a = math.floor(a)
+		for i = 0, a - 1 do
+			for j = 0, bb_length - 1 do
+				setpixel(x1 - i + j, y1 + i + j, r, g, b) --primary pixels
+			end
+			for j = 0, bb_length - 2 do
+				setpixel(x1 - i + j, y1 + i + j + 1, r, g, b) --in-between fill pixels
+			end
 		end
 	end
+
+	local function circle(cx, cy, radius, thickness, r, g, b)
+		for i = -thickness / 2, thickness / 2 do
+			for a = 0, 2 * math.pi, 0.001 do
+				setpixel(
+					math.floor(cx + math.cos(a) * (radius + i)),
+					math.floor(cy + math.sin(a) * (radius + i)),
+					r, g, b)
+			end
+		end
+	end
+
+	local function draw_x(x, y, bb_length)
+		--
+	end
+
+	local function draw_zero()
+		--
+	end
+
+	rectangle(0, 0, bmp.w, bmp.h, 0, 0, 0)
+	vline(10, 10, 100, 10, 255, 255, 255)
+	diagonal(10, 10, 100, 10, 255, 255, 255)
+	circle(110, 110, 100, 10, 255, 255, 255)
+
 end
 
 app:run()
