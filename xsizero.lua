d97f79bb9fb2fa2d20648522ce2003662dc915ba
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2016-02-08 18:24:56 +0200

    draw_x and draw_zero; rectangle centering; refactoring: clipping setpixel to avoid access violation; math.floor on setpixel

--- xsizero.lua
+++ xsizero.lua
@@ -141,16 +141,16 @@ function win:repaint()
 	local p = ffi.cast('uint8_t*', bmp.data)
 
 	local function setpixel(x, y, r, g, b)
+		x = math.floor(x)
+		y = math.floor(y)
+		if x > bmp.w - 1 then return end
+		if y > bmp.h - 1 then return end
 		p[(y * bmp.w + x) * 4 + 2] = r
 		p[(y * bmp.w + x) * 4 + 1] = g
 		p[(y * bmp.w + x) * 4 + 0] = b
 	end
 
 	local function rectangle(x0, y0, w, h, r, g, b)
-		assert(x0 < bmp.w)
-		assert(y0 < bmp.h)
-		assert(x0 + w <= bmp.w)
-		assert(y0 + h <= bmp.h)
 		for y = y0, y0 + h - 1 do
 			for x = x0, x0 + w - 1 do
 				setpixel(x, y, r, g, b)
@@ -159,18 +159,18 @@ function win:repaint()
 	end
 
 	local function vline(x0, y0, length, thickness, r, g, b)
-		rectangle(x0 - math.floor(thickness / 2), y0, thickness, length, r, g, b)
+		rectangle(x0 - thickness / 2, y0, thickness, length, r, g, b)
 	end
 
-	local function hline(x, y, length, r, g, b)
-		rectangle(x0, y0 - math.floor(thickness / 2), length, thickness, r, g, b)
+	local function hline(x0, y0, length, thickness, r, g, b)
+		rectangle(x0, y0 - thickness / 2, length, thickness, r, g, b)
 	end
 
-	local function diagonal(x0, y0, bb_length, thickness, r, g, b)
+	local function diagonal1(x0, y0, bb_length, thickness, r, g, b)
 		local c = thickness
 		local a = math.sqrt(c^2 / 2)
-		local x1 = x0 + math.floor(a / 2)
-		local y1 = y0 - math.floor(a / 2)
+		local x1 = x0 + a / 2
+		local y1 = y0 - a / 2
 		a = math.floor(a)
 		for i = 0, a - 1 do
 			for j = 0, bb_length - 1 do
@@ -182,29 +182,75 @@ function win:repaint()
 		end
 	end
 
+	local function diagonal2(x0, y0, bb_length, thickness, r, g, b)
+		local c = thickness
+		local a = math.sqrt(c^2 / 2) --teorema lui pitagora
+		local x1 = x0 + a / 2
+		local y1 = y0 - a / 2
+		a = math.floor(a) --a is from a^2 + a^2 = c^2
+		for i = 0, a - 1 do
+			for j = 0, bb_length - 1 do
+				setpixel(x1 + i - j, y1 + i + j, r, g, b) --primary pixels
+			end
+			for j = 0, bb_length - 2 do
+				setpixel(x1 + i - j, y1 + i + j + 1, r, g, b) --in-between fill pixels
+			end
+		end
+	end
+
 	local function circle(cx, cy, radius, thickness, r, g, b)
 		for i = -thickness / 2, thickness / 2 do
 			for a = 0, 2 * math.pi, 0.001 do
 				setpixel(
-					math.floor(cx + math.cos(a) * (radius + i)),
-					math.floor(cy + math.sin(a) * (radius + i)),
+					cx + math.cos(a) * (radius + i),
+					cy + math.sin(a) * (radius + i),
 					r, g, b)
 			end
 		end
 	end
 
-	local function draw_x(x, y, bb_length)
-		--
+	--center a rectangle in another
+	local function center_rect(w1, h1, x, y, w, h)
+		local x1 = (w - w1) / 2 + x
+		local y1 = (h - h1) / 2 + y
+		return x1, y1
 	end
 
-	local function draw_zero()
-		--
+	--transform a point from (3*3) game space to (w*h) window space
+	local function transform_point(x, y, x1, y1, w1, h1)
+		local x2 = x / 3 * w1 - w1 / 6 + x1
+		local y2 = y / 3 * h1 - h1 / 6 + y1
+		return x2, y2
 	end
 
-	rectangle(0, 0, bmp.w, bmp.h, 0, 0, 0)
-	vline(10, 10, 100, 10, 255, 255, 255)
-	diagonal(10, 10, 100, 10, 255, 255, 255)
-	circle(110, 110, 100, 10, 255, 255, 255)
+	--board rectangle
+	local w1, h1 = 200, 200
+	local x1, y1 = center_rect(w1, h1, 0, 0, bmp.w, bmp.h)
+
+	local function draw_zero(x, y)
+		local cx, cy = transform_point(x, y, x1, y1, w1, h1)
+		circle(cx, cy, 20, 4, 255, 255, 255)
+	end
+
+	local function draw_x(x, y)
+		local t = 50
+		local cx, cy = transform_point(x, y, x1, y1, w1, h1)
+		local x2 = cx - t / 2
+		local y2 = cy - t / 2
+		diagonal1(x2, y2, t, 20, 255, 255, 255)
+		local x2 = cx + t / 2
+		diagonal2(x2, y2, t, 20, 255, 255, 255)
+	end
+
+	rectangle(x1, y1, w1, h1, 20, 20, 20)
+	for x=0,3 do
+		vline(x1 + x * (w1 / 3), y1, h1, 1, 255, 255, 255)
+	end
+	for y=0,3 do
+		hline(x1, y1 + y * (h1 / 3), w1, 1, 255, 255, 255)
+	end
+	draw_zero(2, 2)
+	draw_x(3, 3)
 
 end
 
